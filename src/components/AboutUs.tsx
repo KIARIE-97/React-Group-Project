a6d2920f1dcafa87a16f51712a79ac61931f9233
maximasprim <michaelmwasame6@gmail.com>
2024-06-28 17:38:53 +0300

    created app-Header and About components

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,34 @@
+// import React from 'react'
+import './About.scss'
+
+function AboutUs() {
+  return (
+    <div className='container'>
+        <div className="container1">
+            <h1>About us</h1>
+            <div className="container1-text">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quae delectus similique rem doloribus porro sint nisi fugiat molestiae architecto ad minima mollitia quaerat possimus nemo dignissimos, sed eligendi dolorem?</p>
+            </div>
+        </div>
+        <div className="container2">
+            <div className="card">
+                <div className="card-text">
+                    <h3>Sales</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id obcaecati nihil adipisci porro necessitatibus consequatur est quo? Voluptates libero, exercitationem, nesciunt corrupti suscipit, ea animi ullam inventore optio nulla doloremque.</p>
+                </div>
+            </div>
+            <div className="card">
+                <h3>Technical support</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates autem dicta possimus a? Ipsam tenetur nobis magni! Excepturi, tempora harum vero rerum vel commodi asperiores minima, ducimus reiciendis suscipit similique.</p>
+            </div>
+            <div className="card">
+                <h3></h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto modi nemo molestiae earum perspiciatis quaerat at fugit molestias dolorem aperiam, eveniet fuga accusantium ipsa excepturi doloribus pariatur tempore dolorum? Quia?</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default AboutUs;
